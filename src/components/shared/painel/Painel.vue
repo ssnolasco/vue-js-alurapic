0d08604508e3f9ac0db030c06b857e95cd552466
Stephannie Silveira <stephanniesilveira@SA8432.local>
2017-09-18 19:12:54 -0300

    esconde foto dbclick -> transição

--- src/components/shared/painel/Painel.vue
+++ src/components/shared/painel/Painel.vue
@@ -1,19 +1,29 @@
 <template>
     <div class="painel">
-        <h2 class="painel-titulo">{{ titulo }}</h2>
-        <slot class="painel-conteudo">
-        </slot>
+        <h2 class="painel-titulo" @dblclick="visivel = !visivel">{{ titulo }}</h2>
+        <transition name="painel-fade">
+          <div class="painel-conteudo" v-show="visivel">
+            <slot></slot>
+          </div>
+        </transition>
     </div>
 </template>
 
 <script>
 export default {
-    props: ['titulo']
+    props: ['titulo'],
+
+    data() {
+      return {
+        visivel: true
+      }
+    },
+
 }
 </script>
 
 <style scoped>
- 
+
    .painel {
     padding: 0 auto;
     border: solid 2px grey;
@@ -33,5 +43,13 @@ export default {
     margin: 0 0 15px 0;
     padding: 10px;
     text-transform: uppercase;
+  }
+
+  .painel-fade-enter, .painel-fade-leave-active {
+    opacity: 0
+  }
+
+  .painel-fade-enter-active, .painel-fade-leave-active {
+    transition: opacity .4s
   }
 </style>
